b453e9e7c0778ec6fcc61dcf5f40c5cd04edebf2
jeremy.shaw <jeremy.shaw@linspireinc.com>
2008-01-23 01:42:41 +0000

    added forceSymbolicLink

---
+++ Linspire/Unix/Files.hs
@@ -0,0 +1,14 @@
+module Linspire.Unix.Files where
+
+import System.Posix.Files
+import System.IO.Error
+
+-- |calls 'createSymbolicLink' but will remove the target and retry if
+-- 'createSymbolicLink' raises EEXIST.
+forceSymbolicLink :: FilePath -> FilePath -> IO ()
+forceSymbolicLink target linkName =
+    createSymbolicLink target linkName `catch`
+      (\e -> if isAlreadyExistsError e 
+             then do removeLink target
+                     createSymbolicLink target linkName
+             else ioError e)
