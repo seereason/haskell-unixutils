c37f5675c2b05c25fd223cc50ae84caae034a6b1
jeremy <jeremy@n-heptane.com>
2008-08-19 20:06:41 +0000

    .cabal tweaks before uploading to hackage

--- System/Unix/SpecialDevice.hs
+++ System/Unix/SpecialDevice.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE PatternSignatures #-}
 -- | Construct an ADT representing block and character devices
 -- (but mostly block devices) by interpreting the contents of
 -- the Linux sysfs filesystem.
