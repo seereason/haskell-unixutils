ce21d9ece3d722a0368f49f3d079928cc074a288
David Fox <dsf@seereason.com>
2010-12-29 18:39:27 +0000

    Add quietness control

--- System/Unix/Progress.hs
+++ System/Unix/Progress.hs
@@ -2,29 +2,46 @@
 {-# OPTIONS -Wall -fno-warn-name-shadowing #-}
 -- |Control the progress reporting and output of subprocesses.
 module System.Unix.Progress
-    ( Progress
+    ( -- * The Progress Monad
+      Progress
     , ProgressFlag(..)
     , runProgress
+    -- * Process launching
     , lazyCommandP
     , lazyProcessP
-    , prefixes
-    , printOutput
-    , dotOutput
+    , lazyCommandV -- Print everything
+    , lazyProcessV
+    , lazyCommandF -- Like V, but throws exception on failure
+    , lazyProcessF
+    , lazyCommandE -- Print everything on failure
+    , lazyProcessE
+    , lazyCommandEF -- E and F combo
+    , lazyProcessEF
+    , lazyCommandD -- Dots
+    , lazyCommandQ -- Quiet
+    , lazyCommandS -- Silent
+    , lazyCommandSF
+    -- * Quietness control
+    , defaultQuietness
+    , modQuietness
+    , quieter
+    -- * Output stream processing
+    -- , prefixes
+    -- , printOutput
+    -- , dotOutput
     , timeTask
     , showElapsed
     , ePutStr
     , ePutStrLn
+    , qPutStr
+    , qPutStrLn
     , eMessage
     , eMessageLn
-    , lazyCommandV -- Verbose
-    , lazyCommandF -- Like V, but fails
-    , lazyProcessF
-    , lazyCommandQ -- Quiet
-    , lazyCommandD -- Dots
-    , quieter
+    , qMessage
+    , qMessageLn
     ) where
 
-import Control.Exception (evaluate {- , try, SomeException -})
+import Control.Exception (evaluate, try, SomeException)
 import Control.Monad (when)
 import Control.Monad.State (StateT, get, evalStateT)
 import "mtl" Control.Monad.Trans ( MonadIO, liftIO, lift )
@@ -33,15 +50,36 @@ import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy.Char8 as L
 import Data.List (intercalate)
 import Data.Time (NominalDiffTime, getCurrentTime, diffUTCTime)
-import System.Environment (getArgs, withArgs)
+import System.Environment (getArgs, getEnv)
 import System.Exit (ExitCode(..))
 import System.IO (hPutStrLn, stderr, hPutStr)
+import System.Posix.Env (setEnv)
 import System.Unix.Process (lazyProcess, lazyCommand, Output(Stdout, Stderr), exitCodeOnly)
 import qualified Data.Set as Set
 
+-- |A monad for controlling progress reporting of subprocesses.
 type Progress a = StateT (Set.Set ProgressFlag) IO a
-data ProgressFlag = Echo | EchoOnFail | Dots | All | AllOnFail | Result | ResultOnFail | FailOnFailure deriving (Ord, Eq)
-runProgress :: [ProgressFlag]  -- ^ Additional flags, such as FailOnFailure
+
+-- |The flags that control what type of output will be sent to stdout
+-- and stderr.  Also, the ExceptionOnFail flag controls whether an
+-- exception will be thrown if the @ExitCode@ is not @ExitSuccess@.
+data ProgressFlag
+    = Echo
+    | Dots
+    | All
+    | Errors
+    | Result
+    | EchoOnFail
+    | AllOnFail
+    | ErrorsOnFail
+    | ResultOnFail
+    | ExceptionOnFail
+    deriving (Ord, Eq)
+
+-- |Run the Progress monad with the given flags.  The flag set is
+-- compute from the current quietness level, <= 0 the most verbose
+-- and >= 4 the least.
+runProgress :: [ProgressFlag]  -- ^ Additional flags, such as FailOnFail
             -> Progress a      -- ^ The progress task to be run
             -> IO a
 runProgress flags action =
@@ -50,29 +88,89 @@ runProgress flags action =
     where
       quietFlags n
           | n <= 0 = [Echo, All, Result]
-          | n == 1 = [Echo, Dots, AllOnFail, Result]
-          | n == 2 = [Echo, AllOnFail, ResultOnFail]
-          | n == 3 = [EchoOnFail, AllOnFail, ResultOnFail]
+          | n == 1 = [Echo, Errors, Result]
+          | n == 2 = [Echo, Dots, Result]
+          | n == 3 = [Echo]
           | True   = []
+
+lazyCommandP :: [ProgressFlag] -> String -> L.ByteString -> IO [Output]
+lazyCommandP flags cmd input = runProgress flags (lift (lazyCommand cmd input) >>= doProgress cmd)
+
+lazyProcessP :: [ProgressFlag] -> FilePath -> [String] -> Maybe FilePath -> Maybe [(String, String)] -> L.ByteString -> IO [Output]
+lazyProcessP flags exec args cwd env input =
+    runProgress flags (lift (lazyProcess exec args cwd env input) >>= doProgress (intercalate " " (exec : args)))
+
+lazyCommandV :: String -> L.ByteString -> IO [Output]
+lazyCommandV = lazyCommandP []
+
+lazyProcessV :: FilePath -> [String] -> Maybe FilePath -> Maybe [(String, String)] -> L.ByteString -> IO [Output]
+lazyProcessV = lazyProcessP []
+
+lazyCommandF :: String -> L.ByteString -> IO [Output]
+lazyCommandF = lazyCommandP [ExceptionOnFail]
+
+lazyProcessF :: FilePath -> [String] -> Maybe FilePath -> Maybe [(String, String)] -> L.ByteString -> IO [Output]
+lazyProcessF = lazyProcessP [ExceptionOnFail]
+
+lazyCommandE :: String -> L.ByteString -> IO [Output]
+lazyCommandE = lazyCommandP [EchoOnFail, AllOnFail, ResultOnFail]
+
+lazyProcessE :: FilePath -> [String] -> Maybe FilePath -> Maybe [(String, String)] -> L.ByteString -> IO [Output]
+lazyProcessE = lazyProcessP [EchoOnFail, AllOnFail, ResultOnFail]
+
+lazyCommandEF :: String -> L.ByteString -> IO [Output]
+lazyCommandEF = lazyCommandP [EchoOnFail, AllOnFail, ResultOnFail, ExceptionOnFail]
+
+lazyProcessEF :: FilePath -> [String] -> Maybe FilePath -> Maybe [(String, String)] -> L.ByteString -> IO [Output]
+lazyProcessEF = lazyProcessP [EchoOnFail, AllOnFail, ResultOnFail, ExceptionOnFail]
+
+lazyCommandD :: String -> L.ByteString -> IO [Output]
+lazyCommandD cmd input = quieter 1 $ lazyCommandP [EchoOnFail, AllOnFail, ResultOnFail] cmd input
+
+lazyCommandQ :: String -> L.ByteString -> IO [Output]
+lazyCommandQ cmd input = quieter 3 $ lazyCommandP [EchoOnFail, AllOnFail, ResultOnFail] cmd input
+
+lazyCommandS :: String -> L.ByteString -> IO [Output]
+lazyCommandS cmd input = quieter 4 $ lazyCommandP [EchoOnFail, AllOnFail, ResultOnFail] cmd input
+
+lazyCommandSF :: String -> L.ByteString -> IO [Output]
+lazyCommandSF cmd input = quieter 4 $ lazyCommandP [EchoOnFail, AllOnFail, ResultOnFail, ExceptionOnFail] cmd input
+
+-- |Look for occurrences of -v and -q in the command line arguments
+-- and the current values of environment variables VERBOSITY and
+-- QUIETNESS to compute a new value for QUIETNESS.  If you want to
+-- ignore the current QUIETNESS value say @setQuietness 0 >>
+-- computeQuietness@.
+defaultQuietness :: IO Int
+defaultQuietness =
+    do v1 <- try (getEnv "VERBOSITY" >>= return . read) >>= either (\ (_ :: SomeException) -> return 0) return
+       v2 <- getArgs >>= return . length . filter (== "-v")
+       q1 <- try (getEnv "QUIETNESS" >>= return . read) >>= either (\ (_ :: SomeException) -> return 0) return
+       q2 <- getArgs >>= return . length . filter (== "-q")
+       return $ q1 - v1 + q2 - v2
 
 -- |Look at the number of -v and -q arguments to get the baseline
 -- quietness / verbosity level for progress reporting.
 quietness :: IO Int
-quietness =
-    do -- v1 <- try (getEnv "VERBOSITY" >>= return . read) >>= either (\ (_ :: SomeException) -> return 0) return
-       v2 <- getArgs >>= return . length . filter (== "-v")
-       -- q1 <- try (getEnv "QUIETNESS" >>= return . read) >>= either (\ (_ :: SomeException) -> return 0) return
-       q2 <- getArgs >>= return . length . filter (== "-q")
-       return ({- v1 - q1 + -} q2 - v2)
+quietness = try (getEnv "QUIETNESS" >>= return . read) >>= either (\ (_ :: SomeException) -> return 0) return
+
+-- |Perform a task with the given quietness level.
+modQuietness :: (Int -> Int) -> IO a -> IO a
+modQuietness f task =
+    quietness >>= \ q0 ->
+    setQuietness (f q0) >>
+    task >>= \ result ->
+    setQuietness q0 >>
+    return result
+    where
+      -- Set the value of QUIETNESS in the environment.
+      setQuietness :: Int -> IO ()
+      setQuietness q = setEnv "QUIETNESS" (show q) True
 
 -- |Do an IO task with additional -v or -q arguments so that the
 -- progress reporting becomes more or less verbose.
 quieter :: Int -> IO a -> IO a
-quieter n task =
-    do args <- getArgs
-       withArgs (extra ++ args) task
-    where
-      extra = replicate n (if n < 0 then "-v" else "-q")
+quieter q task = modQuietness (+ q) task
 
 -- |Inject a command's output into the Progress monad, handling command echoing,
 -- output formatting, result code reporting, and exception on failure.
@@ -82,33 +180,24 @@ doProgress cmd output =
     doEcho s output >>= doOutput s >>= doResult s >>= doFail s
     where
       doEcho s output
-          | Set.member Echo s =
+          | Set.member Echo s || (Set.member EchoOnFail s && exitCodeOnly output /= ExitSuccess) =
               lift (hPutStrLn stderr ("-> " ++ cmd)) >> return output
-          | Set.member EchoOnFail s =
-              case exitCodeOnly output of
-                ExitSuccess -> return output
-                _ -> lift (hPutStrLn stderr ("-> " ++ cmd)) >> return output
           | True = return output
       doOutput s output
-          | Set.member All s =
+          | Set.member All s || (Set.member AllOnFail s && exitCodeOnly output /= ExitSuccess) =
               lift (printOutput (prefixes opre epre output))
-          | Set.member AllOnFail s =
-              -- This will force all the output
-              case exitCodeOnly output of
-                ExitSuccess -> return output
-                _ ->
-                    -- Make sure the command gets echoed if we fail
-                    when (not (Set.member Echo s)) (lift (hPutStrLn stderr ("> " ++ cmd))) >>
-                    lift (printOutput (prefixes opre epre output))
-          | Set.member Dots s = lift (dotOutput 128 output)
+          | Set.member Dots s =
+              lift (dotOutput 128 output)
+          | Set.member Errors s || (Set.member ErrorsOnFail s && exitCodeOnly output /= ExitSuccess) =
+              lift (printErrors (prefixes opre epre output))
           | True = return output
-      doResult s output =
-          when (Set.member Result s) 
-               (lift (hPutStrLn stderr ("<- " ++ show (exitCodeOnly output)))) >>
-          return output
+      doResult s output
+          | Set.member Result s || (Set.member ResultOnFail s && exitCodeOnly output /= ExitSuccess) =
+              lift (hPutStrLn stderr ("<- " ++ show (exitCodeOnly output))) >> return output
+          | True = return output
       doFail :: Set.Set ProgressFlag -> [Output] -> Progress [Output]
       doFail s output
-          | Set.member FailOnFailure s =
+          | Set.member ExceptionOnFail s =
               case exitCodeOnly output of
                 ExitSuccess -> return output
                 result -> fail ("*** FAILURE: " ++ cmd ++ " -> " ++ show result)
@@ -155,13 +244,24 @@ prefixes opre epre output =
                    (s', bol') = doOutput False pre b in 
                (B.append x s', bol')
 
--- |Print all the output to the appropriate output channel
+-- |Print all the output to the appropriate output channel.  Each pair
+-- is the original input (to be returned) and the modified input (to
+-- be printed.)
 printOutput :: [(Output, Output)] -> IO [Output]
 printOutput output =
     mapM print' output
     where
       print' (x, y) = print y >> return x
       print (Stdout s) = putStr (B.unpack s)
+      print (Stderr s) = hPutStr stderr (B.unpack s)
+      print _ = return ()
+
+-- |Print all the error output to the appropriate output channel
+printErrors :: [(Output, Output)] -> IO [Output]
+printErrors output =
+    mapM print' output
+    where
+      print' (x, y) = print y >> return x
       print (Stderr s) = hPutStr stderr (B.unpack s)
       print _ = return ()
 
@@ -195,38 +295,35 @@ formatTime' diff = show diff
       toMilliseconds f = round (f * 1000)
 -}
 
+-- |Send a string to stderr.
 ePutStr :: String -> IO ()
 ePutStr = hPutStr stderr
 
+-- |@ePutStr@ with a terminating newline.
 ePutStrLn :: String -> IO ()
 ePutStrLn = hPutStrLn stderr
 
--- |Print a message without forcing the command's output
+-- |If the current quietness level is less than one print a message.
+-- Control the quietness level using @quieter@.
+qPutStr :: String -> IO ()
+qPutStr s = quietness >>= \ q -> when (q <= 0) (ePutStr s)
+
+-- |@qPutStr@ with a terminating newline.
+qPutStrLn :: String -> IO ()
+qPutStrLn s = quietness >>= \ q -> when (q <= 0) (ePutStrLn s)
+
+-- |Print a message and return the second argument unevaluated.
 eMessage :: String -> a -> IO a
 eMessage message output = ePutStr message >> return output
 
--- |Print a message without forcing the command's output
+-- |@eMessage@ with a terminating newline.
 eMessageLn :: String -> a -> IO a
 eMessageLn message output = ePutStrLn message >> return output
 
-lazyCommandP :: [ProgressFlag] -> String -> L.ByteString -> IO [Output]
-lazyCommandP flags cmd input = runProgress flags (lift (lazyCommand cmd input) >>= doProgress cmd)
+-- |@eMessage@ controlled by the quietness level.
+qMessage :: String -> a -> IO a
+qMessage message output = quietness >>= \ q -> when (q <= 0) (ePutStr message) >> return output
 
-lazyCommandV :: String -> L.ByteString -> IO [Output]
-lazyCommandV = lazyCommandP []
-
-lazyCommandF :: String -> L.ByteString -> IO [Output]
-lazyCommandF = lazyCommandP [FailOnFailure]
-
-lazyProcessP :: [ProgressFlag] -> FilePath -> [String] -> Maybe FilePath -> Maybe [(String, String)] -> L.ByteString -> IO [Output]
-lazyProcessP flags exec args cwd env input =
-    runProgress flags (lift (lazyProcess exec args cwd env input) >>= doProgress (intercalate " " (exec : args)))
-
-lazyProcessF :: FilePath -> [String] -> Maybe FilePath -> Maybe [(String, String)] -> L.ByteString -> IO [Output]
-lazyProcessF = lazyProcessP [FailOnFailure]
-
-lazyCommandD :: String -> L.ByteString -> IO [Output]
-lazyCommandD cmd input = quieter 1 $ lazyCommandP [EchoOnFail, AllOnFail, ResultOnFail] cmd input
-
-lazyCommandQ :: String -> L.ByteString -> IO [Output]
-lazyCommandQ cmd input = quieter 3 $ lazyCommandP [EchoOnFail, AllOnFail, ResultOnFail] cmd input
+-- |@qMessage@ with a terminating newline.
+qMessageLn :: String -> a -> IO a
+qMessageLn message output = quietness >>= \ q -> when (q <= 0) (ePutStrLn message) >> return output
