031671ddbf439ad0af27ec1df7e260830e835592
David Fox <dsf@seereason.com>
2012-03-14 15:49:15 +0000

    Remove code to run test script, that script is now in progress

--- Setup.hs
+++ Setup.hs
@@ -4,11 +4,4 @@ import Distribution.Simple
 import System.Cmd
 import System.Exit
 
-main = defaultMainWithHooks simpleUserHooks {
-         postBuild = runTestScript
-       , runTests = runTestScript
-       }
-
-runTestScript _args _flag _pd _lbi =
-    system "runhaskell Tests.hs" >>=
-    \ code -> if code == ExitSuccess then return () else error "Test Failure"
+main = defaultMainWithHooks simpleUserHooks
